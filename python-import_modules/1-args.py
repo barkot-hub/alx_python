3ecd4cf004efe14f572672d62ef0bd82d40277f8
barkot-hub <batkotmuhaba62@gmail.com>
2023-07-26 19:11:50 +0300

    arguments

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,8 +1,10 @@
-#import sys
+import sys
 
 args = sys.argv[1:]
 num_args = len(args)
 print("Number of argument" + ("s" if num_args != 1 else "") + ": " + str(num_args) + ("" if num_args == 0 else ":"))
 print()
 for i in range(num_args):
-    print(str(i+1) + ": " + args[i])
+print(str(i+1) + ": " + args[i])
+
+
