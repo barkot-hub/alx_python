066700f6d2239d6ad6472a277f1f72a26b9cd8f4
barkot-hub <batkotmuhaba62@gmail.com>
2023-07-31 09:39:57 +0300

    hguyuyyyl

---
+++ python-more_data_structures/1-common_elements.py
@@ -0,0 +1,7 @@
+def common_elements(set_1, set_2):
+    common_set=()
+        for elem in set_1:
+            if elem in set_2:
+                common_set.add(elem)
+           return common_set     
+    
